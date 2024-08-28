70de846042828b327cb40003339771ca01b1a1c7
Sourav <sourav2xtreme@gmail.com>
2024-08-28 11:51:02 +0530

    Eposide 2

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
-const parent = React.createElement( "div", { id: "parent" },
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" },
     [
-        React.createElement("div",{ id: "child1" },
+        React.createElement("div", { id: "child1" },
             [
-                React.createElement("h1", {}, "Heading 1"),
-                React.createElement("h2", {}, "Heading 2")
+                React.createElement("h1", {}, "JavaScript"),
+                React.createElement("h2", {}, "Master it from Namaste JavaScript")
             ]
         ),
-        React.createElement("div",{ id: "child1" },
+        React.createElement("div", { id: "child1" },
             [
-                React.createElement("h1", {}, "Heading 1"),
-                React.createElement("h2", {}, "Heading 2")
+                React.createElement("h1", {}, "Namaste React"),
+                React.createElement("h2", {}, "Master it from Namaste React")
             ]
         )
     ]
-); 
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
